cb4d65267d17096a895861853a12aeabc3e5d916
lsmin1021 <dltmdals826@gmail.com>
2025-05-08 13:00:10 +0900

    Login UT

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,17 +4,25 @@ using std::string;
 
 TEST(StockBrocker, selectStockBrockerKiwer) {
 	// TODO StockBrockerApp 개발
-	//StockBrokerApp app;
+	StockBrokerApp* app = nullptr;
 
-	//EXPECT_NO_THROW(app.selectStockBrocker("Kiwer"));
+	EXPECT_NO_THROW(app->selectStockBrocker("Kiwer"));
 }
 
 TEST(StockBrocker, selectStockBrockerNemo) {
 	// TODO StockBrockerApp 개발
-	//StockBrokerApp app;
+	StockBrokerApp* app = nullptr;
 
-	//EXPECT_NO_THROW(app.selectStockBrocker("Nemo"));
+	EXPECT_NO_THROW(app.selectStockBrocker("Nemo"));
 }
+
+TEST(StockBrocker, StockBrockerLogin) {
+	// TODO Login 기능
+	StockBrokerApp* app = nullptr;
+	
+	EXPECT_NO_THROW(app.login("idid","pwpw"));
+}
+
 
 int main() {
 	::testing::InitGoogleMock();
