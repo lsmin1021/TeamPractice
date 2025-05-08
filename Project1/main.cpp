351f28d6a1c35a41a2fa944cf1ece7c79fb87777
lsmin1021 <dltmdals826@gmail.com>
2025-05-08 14:05:27 +0900

    sell UT

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -42,6 +42,27 @@ TEST(StockBrocker, StockBrockerLoginFail) {
 	EXPECT_EQ(false, app.login("idid", "pwpw"));
 }
 
+TEST(StockBrocker, StockBrockerSellSucess) {
+	// TODO 매수 기능
+	StockBrockerApp* app = new StockBrockerApp();
+
+	int code = 123;
+	int price = 100;
+	int count = 3;
+	EXPECT_NO_THROW(app->buy(code, price, count));
+}
+
+TEST(StockBrocker, StockBrockerSellSucess) {
+	// TODO 매수 기능
+	StockBrockerApp* app = new StockBrockerApp();
+
+	int code = 123;
+	int price = 0;
+	int count = 3;
+	EXPECT_THROW(app->buy(code, price, count), std::exception);
+}
+
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
