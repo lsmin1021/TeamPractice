6745a6a8e40ba33591998863e8b3849b14981445
changhee <jaduwvad2@gmail.com>
2025-05-08 13:19:09 +0900

    create selectStockBrocker method

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,28 +1,30 @@
 ﻿#include "gmock/gmock.h"
-#include "string"
-using std::string;
+#include "stock_brocker.h"
+
+using namespace testing;
 
 TEST(StockBrocker, selectStockBrockerKiwer) {
 	// TODO StockBrockerApp 개발
-	StockBrokerApp* app = nullptr;
+	StockBrockerApp* app = nullptr;
 
 	EXPECT_NO_THROW(app->selectStockBrocker("Kiwer"));
 }
 
 TEST(StockBrocker, selectStockBrockerNemo) {
 	// TODO StockBrockerApp 개발
-	StockBrokerApp* app = nullptr;
+	StockBrockerApp* app = nullptr;
 
-	EXPECT_NO_THROW(app.selectStockBrocker("Nemo"));
+	EXPECT_NO_THROW(app->selectStockBrocker("Nemo"));
 }
 
+/*
 TEST(StockBrocker, StockBrockerLogin) {
 	// TODO Login 기능
 	StockBrokerApp* app = nullptr;
 	
 	EXPECT_NO_THROW(app.login("idid","pwpw"));
 }
-
+*/
 
 int main() {
 	::testing::InitGoogleMock();
