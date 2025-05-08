fcceb78ae153da4b070bcb0ace32fe11447ba89e
lsmin1021 <dltmdals826@gmail.com>
2025-05-08 12:54:41 +0900

    selectStockBrocker UT 생성

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,21 @@
-#include "gmock/gmock.h"
+﻿#include "gmock/gmock.h"
+#include "string"
+using std::string;
+
+TEST(StockBrocker, selectStockBrockerKiwer) {
+	// TODO StockBrockerApp 개발
+	//StockBrokerApp app;
+
+	//EXPECT_NO_THROW(app.selectStockBrocker("Kiwer"));
+}
+
+TEST(StockBrocker, selectStockBrockerNemo) {
+	// TODO StockBrockerApp 개발
+	//StockBrokerApp app;
+
+	//EXPECT_NO_THROW(app.selectStockBrocker("Nemo"));
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
